70dd6140feeb7509c0106962749d325a35f6e460
Văn Hiếu <vanhieu99it@gmail.com>
2024-06-19 10:02:15 +0700

    add spring-eureka-server

---
+++ discovery-server/spring-eureka-server/src/main/java/com/example/spring_eureka_server/SpringEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaServerApplication.class, args);
+	}
+
+}

72fc2c5012933543e9e37ebf0ceb91990a76485e
Văn Hiếu <vanhieu99it@gmail.com>
2024-10-01 14:08:42 +0700

    up

--- SpringCloudDemo/orderservice/src/main/java/com/example/orderservice/OrderController.java
+++ SpringCloudDemo/orderservice/src/main/java/com/example/orderservice/OrderController.java
@@ -1,8 +1,6 @@
 package com.example.orderservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,19 +10,18 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+
+    private final RestTemplate restTemplate;
+
     @Autowired
-    private RestTemplate restTemplate;
+    public OrderController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping("/{id}")
     public String getOrderById(@PathVariable("id") String orderId) {
         String userInfo = restTemplate.getForObject("http://user-service/users/" + orderId, String.class);
         return "Order info for order id: " + orderId + ", " + userInfo;
     }
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
 }
+
